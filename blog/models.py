740a02bb090def9e65e54b4cc3ed26b9f8b8e74d
Sumit Chawla <sam.chawla26@gmail.com>
2022-01-05 09:33:00 +0530

    DAY 2 Learnings
    Learnings:
    - Understand Models.
    - Created Tag and Post Model
    - Migrations
    - Registering Models on Admin Site

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+User = settings.AUTH_USER_MODEL
+
+
+class Tag(models.Model):
+    value = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.value
+
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('DRAFT', 'Draft'),
+        ('PUBLISHED', 'Published'),
+        ('ARCHIVED', 'Archived')
+    )
+ 
+    title = models.TextField(max_length=125)
+    content = models.TextField()
+    summary = models.TextField(max_length=500, blank=True, null=True)
+    slug = models.SlugField(max_length=125)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    tags = models.ManyToManyField(Tag, related_name="posts")    
+    status = models.CharField(max_length=12, choices=STATUS_CHOICES, default='DRAFT')
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
