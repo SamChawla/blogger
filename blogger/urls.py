32a8b6257578f05d07a2394dd06a88f813fc2213
Sumit Chawla <sam.chawla26@gmail.com>
2022-01-19 09:28:40 +0530

    created custom user model
    - Types of Custom User Model
    - Created accounts app
    - Created Custom User Model using Abstract Base User
    - Created Model Manager for Custom User
    - Updated settings to point to custom user model

--- blogger/urls.py
+++ blogger/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Blogger Admin"
+admin.site.site_title = "Blogger Admin"
+admin.site.index_title = "Blogger"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
