895e95a9d8988f08c092cb946375cb8aecb4917e
Sumit Chawla <sam.chawla26@gmail.com>
2022-01-31 09:19:53 +0530

    Started with Class based Views

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from blog.views import home_view
+from blog.views import (
+    home_view,
+    post_list_view,
+    post_detail_view,
+)
 
 urlpatterns = [
     path("",home_view, name="home"),
+    path("posts/", post_list_view, name="blog-home"),
+    path("posts/<int:pk>/", post_detail_view, name="blog-post"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from blog.models import Post
+from django.views.generic import (
+    CreateView,
+    DetailView,
+    ListView,
+    UpdateView,
+    DeleteView,
+)
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin
+    )
 # Create your views here.
+
 
 def home_view(request, *args, **kwargs):
     posts = Post.objects.filter(status="PUBLISHED")
@@ -10,6 +22,33 @@ def home_view(request, *args, **kwargs):
         "posts": posts,
     }
     return render(request, "blog/index.html", context_data)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/index.html"
+    queryset = Post.objects.filter(status="PUBLISHED")
+    context_object_name = "posts"
+    ordering = ["-modified_at"]
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data()
+        data["title"] = "Home Page | Posts"
+        return data
+
+post_list_view = PostListView.as_view()
+
+class PostDetailView(DetailView):
+    model = Post
+    context_object_name = "post"
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data()
+        data["title"] = ""
+        return data
+
+post_detail_view = PostDetailView.as_view()
 
 
 
